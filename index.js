85e0aa89dc80c5e51b2089dd1349c9e2ad3d33e8
rasedul99 <rasedul2199@gmail.com>
2022-02-25 01:03:14 +0600

    data visualize  in card

--- index.js
+++ index.js
@@ -6,5 +6,27 @@ const searchFood = () => {
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayMeals(data.meals));
+};
+const displayMeals = (data) => {
+  const searchResult = document.getElementById("search-result");
+  data.forEach((meal) => {
+    console.log(meal);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">
+        ${meal.strInstructions.slice(0, 250)}
+      </p>
+    </div>
+  </div> 
+    
+    `;
+
+    searchResult.appendChild(div);
+  });
 };
